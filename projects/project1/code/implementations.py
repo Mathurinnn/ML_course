520f2bfed718f86495fa62df656d726961c7779b
Mathunter <mathurin.verclytte@epfl.ch>
2024-09-24 23:26:56 +0200

    mean_squared_error_gd works

--- projects/project1/code/implementations.py
+++ projects/project1/code/implementations.py
@@ -4,7 +4,6 @@ def least_squares(y, tx):
 
     N = tx.shape[0]
     gram_matrix = tx.T @ tx
-    print(gram_matrix.shape)
     w = np.linalg.solve(gram_matrix, tx.T @ y)
 
     error = y - (tx @ w)
@@ -19,9 +18,9 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 
     def mean_squared_error_gd_recursive(y, tx, w, max_iters, gamma):
         if(max_iters == 0):
-            return (initial_w, (1/2*n) * np.sum(np.square(y - tx @ w))[0]) 
+            return (initial_w, (1/(2*n)) * np.sum(np.square(y - tx @ w)))
         else:
             grad = -(1/n) * (np.transpose(tx) @ (y - tx @ w))#gradient of the loss function
             return mean_squared_error_gd(y, tx, w - gamma * grad, max_iters - 1, gamma)
             
-    mean_squared_error_gd_recursive(y, tx, initial_w, max_iters, gamma)
+    return mean_squared_error_gd_recursive(y, tx, initial_w, max_iters, gamma)
--- projects/project1/code/main.py
+++ projects/project1/code/main.py
@@ -1,8 +1,24 @@
+import numpy as np
+
 from projects.project1.helpers import load_csv_data
 from projects.project1.code.implementations import *
 
+def test_mean_squared_error_gd(y, tx, initial_w):
+    w, loss = mean_squared_error_gd(
+        y, tx, initial_w, 2, 0.1
+    )
+
+    expected_w = np.array([-0.050586, 0.203718])
+    expected_loss = 0.051534
+
+    print(loss, expected_loss)
+    print(w, expected_w)
+
+
+
 if __name__ == '__main__':
 
-    x_train, x_test, y_train, train_ids, test_ids = load_csv_data("C:\\Users\mathu\PycharmProjects\ML_course\projects\project1\data\dataset\dataset")
+    #x_train, x_test, y_train, train_ids, test_ids = load_csv_data("C:\\Users\mathu\PycharmProjects\ML_course\projects\project1\data\dataset\dataset")
 
-    print(least_squares(y_train, x_train))
+
+    print(test_mean_squared_error_gd(np.array([0.1, 0.3, 0.5]), np.array([[2.3, 3.2], [1.0, 0.1], [1.4, 2.3]]), np.array([0.5, 1.0])))
