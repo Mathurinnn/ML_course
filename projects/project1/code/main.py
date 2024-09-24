76cb996e824084ee2cf48916918abda7651a8128
Mathunter <mathurin.verclytte@epfl.ch>
2024-09-24 22:05:17 +0200

    main

--- projects/project1/code/main.py
+++ projects/project1/code/main.py
@@ -1,10 +1,8 @@
 from projects.project1.helpers import load_csv_data
-
+from projects.project1.code.implementations import *
 
 if __name__ == '__main__':
 
     x_train, x_test, y_train, train_ids, test_ids = load_csv_data("C:\\Users\mathu\PycharmProjects\ML_course\projects\project1\data\dataset\dataset")
-
-
 
 
