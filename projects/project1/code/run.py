f40b1d712099fd9e0da2049de34afc13d0d5de59
aitor <aitor.deschryver-larralde@epfl.ch>
2024-10-30 10:42:17 +0100

    7

--- projects/project1/code/run.py
+++ projects/project1/code/run.py
@@ -1,6 +1,7 @@
 import numpy as np
 from dask.array.random import logistic
 
+from projects.project1.code.clean_data import clean_data
 from projects.project1.code.cross_validation import build_k_indices, cross_validation, general_cross_validation, \
     logistic_cross_validation
 from projects.project1.code.implementations import logistic_regression
@@ -8,33 +9,8 @@ from projects.project1.helpers import load_csv_data
 
 x_train, x_test, y_train, train_ids, test_ids = load_csv_data("/Users/deschryver/PycharmProjects/ML_course/projects/project1/data/dataset")
 
-columnnsToRemove = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,52,53,54,55,56,57,58,59,60,62,63,64,119,120,121,123,122,124,125,126,127,128,129,130,131,132,133,134,135,136,178,195,197,199,200,201,202,203,204,217,218,219,220,221,222,223,224,225,226,227,228,229,230, 240,241,242,243,244,245,246,251]
-not_seve_or_nine = [0, 51, 60, 103, 107, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 281,282,283,284,286,287,290,291,292,293,296,297.298,300,301,302,303,304,305]
-#78, 94 garder 888,
-seven_nine_hundred =[78, 82,83,84,85,86,87,90,91, 93, 94, 111, 144, 151]
-
-seventy_seven = [76, 79, 80,81, 99, 113,169]
-
-number_of_columns = x_train.shape[1]
-number_of_lines = x_train.shape[0]
-
-for l in range(0, number_of_lines - 1):
-    for r in range(0, number_of_columns - 1):
-        if (not (r in not_seve_or_nine)):
-            if(x_train[l,r] == 7 or x_train[l,r] == 9 or x_train[l,r] == 8):
-                x_train[l,r] = 0
-            if (r in seven_nine_hundred):
-                    if(x_train[l,r] == 777 or x_train[l,r] == 999):
-                        x_train[l,r] = 0
-                    if(r != 78 and r != 94):
-                        if(x_train[l,r] == 888):
-                            x_train[l,r] = 0
-            if(r in seventy_seven):
-                if(x_train[l,r] == 77 or x_train[l,r] == 99):
-                    x_train[l,r] = 0
-
-x_train = np.delete(x_train, columnnsToRemove, axis=1)
-x_test = np.delete(x_test, columnnsToRemove, axis=1)
+x_train = clean_data(x_train)
+x_test = clean_data(x_test)
 
 #on remplace les nan par des 0
 x_train = np.nan_to_num(x_train)
