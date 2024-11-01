2652c03b821775dbb883f64468bcc4c9fa955d74
aitor <aitor.deschryver-larralde@epfl.ch>
2024-11-01 02:36:28 +0100

    7

--- implementations.py
+++ implementations.py
@@ -97,10 +97,9 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     w = initial_w
-
     for n_iter in range(max_iters):
         grad = compute_gradient_logistic(y, tx, w) + 2 * lambda_ * w
-        w = w - gamma * grad + lambda_ * np.sum(w)
+        w = w - gamma * grad
 
     loss = compute_logistic_loss(y, tx, w)
     return w, loss
