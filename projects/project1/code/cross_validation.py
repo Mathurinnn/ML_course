8d11cd7e389a6c72c8002cc0ff74cd21faf3c3b2
aitor <aitor.deschryver-larralde@epfl.ch>
2024-11-01 15:05:30 +0100

    7

--- projects/project1/code/cross_validation.py
+++ projects/project1/code/cross_validation.py
@@ -3,6 +3,8 @@ from dask.array.random import logistic
 
 from implementations import *
 from build_poly import *
+from projects.project1.code.compute_gradient import sigmoid
+
 
 def cross_validation(y, x, k_indices, k, lambda_, degree):
     """return the loss of ridge regression for a fold corresponding to k_indices
@@ -18,10 +20,6 @@ def cross_validation(y, x, k_indices, k, lambda_, degree):
     Returns:
         train and test root mean square errors rmse = sqrt(2 mse)
     """
-
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # get k'th subgroup in test, others in train:
     x_train = np.copy(x)
     y_train = np.copy(y)
 
@@ -55,10 +53,6 @@ def build_k_indices(y, k_fold, seed):
 
     Returns:
         A 2D array of shape=(k_fold, N/k_fold) that indicates the data indices for each fold
-
-    >>> build_k_indices(np.array([1., 2., 3., 4.]), 2, 1)
-    array([[3, 2],
-           [0, 1]])
     """
     num_row = y.shape[0]
     interval = int(num_row / k_fold)
@@ -67,61 +61,28 @@ def build_k_indices(y, k_fold, seed):
     k_indices = [indices[k * interval : (k + 1) * interval] for k in range(k_fold)]
     return np.array(k_indices)
 
-
-def general_cross_validation(y, x, k_indices, k, function, args):
-    """return the loss of function for a fold corresponding to k_indices
-
-    Args:
-        y:          shape=(N,)
-        x:          shape=(N,)
-        k_indices:  2D array returned by build_k_indices()
-        k:          scalar, the k-th fold (N.B.: not to confused with k_fold which is the fold nums)
-        lambda_:    scalar, cf. ridge_regression()
-        function:     the tested function
-
-    Returns:
-        train and test root mean square errors rmse = sqrt(2 mse)
-    """
-
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # get k'th subgroup in test, others in train:
-    x_train = np.copy(x)
-    y_train = np.copy(y)
-
-    x_test = np.zeros((k_indices.shape[1], x_train.shape[1]))
-    y_test = np.zeros(x_test.shape[0])
-    x_train = np.delete(x_train, k_indices[k], 0)
-    y_train = np.delete(y_train, k_indices[k], 0)
-
-    j = 0
-    for i in k_indices[k]:
-        x_test[j] = x[i]
-        y_test[j] = y[i]
-        j += 1
-    w = np.zeros(x.shape[1])
-    args.insert(0, w)
-    args.insert(0, x_train)
-    args.insert(0, y_train)
-    w = function(*args)[0]
-    print("************************************w*************************")
-    print(w)
-    loss_tr = compute_loss(y_train, x_train, w)
-    loss_te = compute_loss(y_test, x_test, w)
-
-    return loss_tr, loss_te
-
-
 def logistic_cross_validation(y, x, k_indices, k, max_iter, gamma):
+    """return the loss of logistic regression for a fold corresponding to k_indices
 
+        Args:
+            y:          shape=(N,)
+            x:          shape=(N,)
+            k_indices:  2D array returned by build_k_indices()
+            k:          scalar, the k-th fold (N.B.: not to confused with k_fold which is the fold nums)
+            lambda_:    scalar, cf. ridge_regression()
+            degree:     scalar, cf. build_poly()
+
+        Returns:
+            f1: f1 score
+            loss: the training loss
+            loss_te: the test loss
+        """
     x_train = np.copy(x)
     y_train = np.copy(y)
-
     x_test = np.zeros((k_indices.shape[1], x_train.shape[1]))
     y_test = np.zeros(x_test.shape[0])
     x_train = np.delete(x_train, k_indices[k], 0)
     y_train = np.delete(y_train, k_indices[k], 0)
-
     j = 0
     for i in k_indices[k]:
         x_test[j] = x[i]
@@ -129,11 +90,14 @@ def logistic_cross_validation(y, x, k_indices, k, max_iter, gamma):
         j += 1
     w = np.zeros(x.shape[1])
 
-    w, loss = reg_logistic_regression(y_train, x_train,w,max_iter,gamma)
+    w, loss = logistic_regression(y_train, x_train,w,max_iter, gamma)
 
     loss_te = compute_logistic_loss(y_test, x_test, w)
-
-    return loss, loss_te
+    r = x_test @ w
+    output = sigmoid(r)
+    prediction = np.where(output > 0.5, 1, 0)
+    f1 = compute_f1_score(y_test, prediction)
+    return f1, loss, loss_te
 
 def get_train_test(y, x, k_indices, k):
 
@@ -154,6 +118,20 @@ def get_train_test(y, x, k_indices, k):
     return x_train, y_train, x_test, y_test
 
 def compute_f1_score(y_test, predictions):
+    """
+        Computes the F1 score
+
+        Parameters:
+        y_test : numpy array
+            The true labels for the test set.
+        predictions : numpy array
+            The predicted labels from the model.
+
+        Returns:
+        f1_score : float
+            The computed F1 score.
+        """
+
     true_positive = 0
     true_negative = 0
     false_positive = 0
