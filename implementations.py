07a786297d831c43f51ed8dfb660501001e84fd1
aitor <aitor.deschryver-larralde@epfl.ch>
2024-11-01 15:00:44 +0100

    7

--- implementations.py
+++ implementations.py
@@ -6,7 +6,21 @@ from projects.project1.code.compute_loss import compute_loss, compute_logistic_l
 
 
 def least_squares(y, tx):
+    """
+       Computes the optimal weights using the least squares method.
 
+       Inputs:
+       y : numpy array
+           The target values (labels).
+       tx : numpy array
+           The input features, where each row represents a sample and each column represents a feature.
+
+       Outputs:
+       w : numpy array
+           The optimal weights that minimize the least squares loss.
+       loss : float
+           The computed loss associated with the optimal weights.
+       """
     n = tx.shape[0]
     gram_matrix = tx.T @ tx
     w = np.linalg.solve(gram_matrix, tx.T @ y)
@@ -71,7 +85,7 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     for n_iter in range(max_iters):
         grad = compute_gradient_logistic(y, tx, w)
         w = w - gamma * grad
-        # should there be a loss computation here?
+
     loss = compute_logistic_loss(y, tx, w)
     return w, loss
 
@@ -96,6 +110,29 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """
+        regularized logistic regression using gradient descent.
+
+        Inputs:
+        y : numpy array
+            The target values (labels).
+        tx : numpy array
+            The input features, where each row represents a sample and each column represents a feature.
+        lambda_ : float
+            The regularization parameter that controls the amount of regularization.
+        initial_w : numpy array
+            The initial weights for the logistic regression model.
+        max_iters : int
+            The maximum number of iterations for gradient descent.
+        gamma : float
+            The step size (learning rate) for gradient descent.
+
+        Outputs:
+        w : numpy array
+            The weights that minimize the regularized logistic loss.
+        loss : float
+            The computed loss associated with the weights.
+        """
     w = initial_w
     for n_iter in range(max_iters):
         grad = compute_gradient_logistic(y, tx, w) + 2 * lambda_ * w
